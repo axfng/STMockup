3589087c35c67e7c8a6052f519a001b04a1097ef
Alex Feng <146572860+axfng@users.noreply.github.com>
2024-08-23 15:00:03 -0400

    Initial Commit

---
+++ STMockup/STMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  STMockupApp.swift
+//  STMockup
+//
+//  Created by alfeng on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct STMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
